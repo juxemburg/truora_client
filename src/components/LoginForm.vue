56e9da720398f992b353d1daec3986ab469262fc
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2020-01-10 06:08:50 -0500

    feat(views): added initial login html and style structure

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3 class="mb-5">Iniciar sesión</h3>
+    <b-form>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          type="password"
+          required
+          placeholder="Contraseña"
+        ></b-form-input>
+      </b-form-group>
+
+      <div class="d-flex justify-content-center p-2 bd-highlight">
+        <b-button type="submit" variant="primary">Submit</b-button>
+      </div>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="login-dashboard">
+    <div class="login-dashboard__content">
+      <div class="card">
+        <div class="card-body">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+    <div class="login-dashboard__image">
+      <img alt="Login page banner image" src="../assets/login-banner.svg" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import LoginForm from '@/components/LoginForm.vue';
+
+export default Vue.extend({
+  name: 'Login',
+  components: {
+    LoginForm,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.login-dashboard {
+  height: 100%;
+  display: grid;
+
+  grid-template-areas: 'content image';
+  grid-template-rows: 1fr;
+  grid-template-columns: 6fr 4fr;
+
+  & > {
+    .login-dashboard__content,
+    .login-dashboard__image {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .login-dashboard__content {
+      grid-area: content;
+      background-color: #f6f4fc;
+      padding: 1rem;
+      & > .card {
+        min-width: 80%;
+      }
+    }
+
+    .login-dashboard__image {
+      grid-area: image;
+      & > img {
+        width: 90%;
+      }
+    }
+  }
+}
+
+@media (max-width: 575.98px) {
+  .login-dashboard {
+    grid-template-areas:
+      'image'
+      'content';
+    grid-template-rows: 3fr 7fr;
+    grid-template-columns: 1fr;
+
+    & > {
+      .login-dashboard__image {
+        & > img {
+          width: 50%;
+        }
+      }
+      .login-dashboard__content {
+        & > .card {
+          min-width: 100%;
+        }
+      }
+    }
+  }
+}
+</style>
